f7d6895026618c8f7bebee4a74dfdd34eedd922d
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-15 21:09:07 -0500

    Agregar archivos de proyecto.

---
+++ TareaLab06/TareaLab06/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TareaLab06
+
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Section { get; set; }
+    }
+
+    public class StudentGroup : List<Student>
+    {
+        public string SectionName { get; set; }
+
+        public StudentGroup(string sectionName, List<Student> students)
+        {
+            SectionName = sectionName;
+            AddRange(students);
+        }
+    }
+}
---
+++ TareaLab06/TareaLab06/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaLab06
+
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<StudentGroup> studentGroups = new List<StudentGroup>
+        {
+            new StudentGroup("Sección A", new List<Student>
+            {
+                new Student { Name = "Rebeca", LastName = "Huallpa" , Age = 20 },
+                new Student { Name = "Valeria", LastName = "Centeno", Age = 22 },
+                new Student { Name = "Jesus", LastName = "Gomez", Age = 23 },
+                new Student { Name = "Maria", LastName = "Dominguez", Age = 18 },
+                new Student { Name = "Pedro", LastName = "Guerrero", Age = 18 }
+            }),
+            new StudentGroup("Sección B", new List<Student>
+            {
+                new Student { Name = "Jaime", LastName = "Farfan" , Age = 18 },
+                new Student { Name = "Linder", LastName = "Torres", Age = 20 },
+                new Student { Name = "Bellasol", LastName = "Magna", Age = 19  },
+                new Student { Name = "Romina", LastName = "Rodríguez", Age = 18 },
+                new Student { Name = "Soledad", LastName = "Jimenez", Age = 17 },
+            }),
+            new StudentGroup("Sección C", new List<Student>
+            {
+                new Student { Name = "Carolima", LastName = "Barreto", Age = 24 },
+                new Student { Name = "Paola", LastName = "Ortega", Age = 19 },
+                new Student { Name = "Rosalinda", LastName = "Perez", Age = 20 },
+                new Student { Name = "Jose Antonio", LastName = "Gallardo", Age = 20 },
+                new Student { Name = "Miguel", LastName = "Duran", Age = 22 }
+            }),
+            new StudentGroup("Seccion D", new List<Student>
+            {
+                new Student { Name = "Santiago", LastName = "Sánchez", Age = 18 },
+                new Student { Name = "Florinda", LastName = "Ramos", Age = 22 },
+                new Student { Name = "Victoria", LastName = "Vargas", Age = 21 },
+                new Student { Name = "Paulina", LastName = "Martinez", Age = 19 },
+                new Student { Name = "Leonardo", LastName = "Garcia", Age = 20 }
+            })
+        };
+
+            listView.ItemsSource = studentGroups;
+        }
+
+    }
+}
